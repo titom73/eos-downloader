552b8bc7feb92d11cfb30ae02ec4e74c76c0440b
Thomas Grimonet <tgrimonet@arista.com>
2022-03-24 09:03:51 +0100

    Make: Add Pypi classifier

--- setup.py
+++ setup.py
@@ -26,5 +26,23 @@ setup(
     author="{}".format(eos_downloader.__author__),
     author_email="{}".format(eos_downloader.__email__),
     long_description=long_description,
-    long_description_content_type='text/markdown'
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Information Technology',
+        'Topic :: System :: Software Distribution',
+        'Topic :: Terminals',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ]
 )
