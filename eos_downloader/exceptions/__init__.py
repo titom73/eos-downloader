80b1eecd1c9b61aa7e5b2f34da328ebd500960ee
Thomas Grimonet <thomas.grimonet@gmail.com>
2024-12-02 17:46:38 +0000

    feat(eos_downloader.exceptions): Create initial exceptions

---
+++ eos_downloader/exceptions/__init__.py
@@ -0,0 +1,5 @@
+class AuthenticationError(Exception):
+    """Erxception when authentication fails."""
+
+class AristaServerError(Exception):
+    """Exception returned when an error occured on server side."""
